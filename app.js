b5add0a7cf6e5a4dbbfa279842bf83518453a740
오명훈 <potahog@omyeonghun-ui-MacBookPro.local>
2020-01-23 00:02:43 +0900

    #2.3 Changing Color

--- app.js
+++ app.js
@@ -1,27 +1,49 @@
 const canvas = document.getElementById("jsCanvas");
+const ctx = canvas.getContext("2d");
+const colors = document.getElementsByClassName("jsColor");
+
+canvas.width = 700;
+canvas.height = 700;
+
+ctx.strokeStyle = "#2c2c2c";
+ctx.lineWidth = 2.5;
 
 let painting = false;
 
-
-function stopPainting(){
-    painting = false;
-}
-function onMouseMove(event){
-    const x = event.offsetX;
-    const y = event.offsetY;
+function stopPainting() {
+  painting = false;
 }
 
-function onMouseDown(event){
-    painting = true;
+function startPainting() {
+  painting = true;
 }
 
-function onMouseUp(event){
-    stopPainting();
+function onMouseMove(event) {
+  const x = event.offsetX;
+  const y = event.offsetY;
+  if (!painting) {
+    ctx.beginPath();
+    ctx.moveTo(x, y);
+  } else {
+    ctx.lineTo(x, y);
+    ctx.stroke();
+  }
 }
 
-if(canvas){
-    canvas.addEventListener("mouseenter", onMouseMove);
-    canvas.addEventListener("mousedown", onMouseDown);
-    canvas.addEventListener("mouseup", onMouseUp);
-    canvas.addEventListener("mouseleave", stopPainting);
+function onMouseDown(event) {
+  painting = true;
 }
+
+function changeColor(event){
+    const color = event.target.style.backgroundColor;
+    ctx.strokeStyle = color;
+}
+
+if (canvas) {
+  canvas.addEventListener("mousemove", onMouseMove);
+  canvas.addEventListener("mousedown", startPainting);
+  canvas.addEventListener("mouseup", stopPainting);
+  canvas.addEventListener("mouseleave", stopPainting);
+}
+
+Array.from(colors).forEach(color => color.addEventListener("click", changeColor));
